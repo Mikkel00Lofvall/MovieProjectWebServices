54bf470df0613257a96e9fa8e4c967f175c0d70a
Mikkel Jensen <mikkel.lofvall@hotmail.com>
2024-09-08 23:50:22 +0200

    Add project files.

---
+++ MovieProjectWebServices/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesDatabase;
+using MoviesDatabase.Interfaces;
+
+namespace MovieProjectWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieRepo repo;
+
+        public MovieController(IMovieRepo repo)
+        {
+            this.repo = repo;
+        }
+
+
+        [HttpGet("GetMovies")]
+        public async Task<IActionResult> Get() 
+        {
+            var movies = await repo.GetAllMovies();
+            
+            return Ok(movies);
+        }
+    }
+}
---
+++ MovieProjectWebServices/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesDatabase;
+using MoviesDatabase.Interfaces;
+using MoviesDatabase.Repos;
+
+namespace MovieProjectWebServices
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddScoped<IMovieRepo, MovieRepo>();
+            builder.Services.AddDbContext<MovieDBContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
