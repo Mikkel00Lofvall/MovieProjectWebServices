a470b9b0bc582dc958576b5f2ca9128a15a7e693
Mikkel Jensen <mikkel.lofvall@hotmail.com>
2024-09-27 00:27:10 +0200

    added ticket

---
+++ MovieProjectWebServices/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MoviesDatabase.DTO;
+using MoviesDatabase.Interfaces;
+using MoviesDatabase.Models;
+using MoviesDatabase.Repos;
+using System.Security.Claims;
+
+namespace MovieProjectWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController() { }
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO loginDTO)
+        {
+
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "example")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                
+            };
+
+            HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return Ok(new { success = true });
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok(new { success = true });
+        }
+    }
+
+}
---
+++ MovieProjectWebServices/Controllers/TicketController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesDatabase.DTO;
+using MoviesDatabase.Models;
+using MoviesDatabase.Repos;
+
+namespace MovieProjectWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+
+        private readonly CinemaHallRepository _HallRepository;
+        private readonly MovieRepository _MovieRepository;
+        private readonly TicketRepository _TicketRepository;
+
+        public TicketController(CinemaHallRepository hallRepository, MovieRepository movieRepository, TicketRepository ticketRepository)
+        {
+            _HallRepository = hallRepository;
+            _MovieRepository = movieRepository;
+            _TicketRepository = ticketRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TicketDTO ticket)
+        {
+            if (ticket == null)
+            {
+                (bool result, string message, MovieModel movie) = await _MovieRepository.GetWithId(ticket.MovieID);
+                (result, message, CinemaHallModel hall) = await _HallRepository.GetWithId(ticket.MovieID);
+
+                if (movie != null && hall != null)
+                {
+                    TicketModel ticketModel = new TicketModel()
+                    {
+                        MovieID = ticket.MovieID,
+                        HallID = ticket.HallID,
+                        SeatID = ticket.SeatID,
+                    };
+
+                    (result, message) = await _TicketRepository.Create(ticketModel);
+
+                    if (result)
+                    {
+                        return Ok();
+                    }
+
+                    else return BadRequest(message);
+                }
+
+                else return BadRequest("No Movie Or Hall With Those IDs");
+            }
+
+            else return BadRequest("No Ticket Data");
+        }
+    }
+}
